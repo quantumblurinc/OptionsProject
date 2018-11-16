f3e318805c0d42c35c1a0b3550e93929647b6f93
quantumblurinc <31797536+quantumblurinc@users.noreply.github.com>
2018-11-16 11:33:29 -0500

    Update OptionsContract.py

--- OptionsContract.py
+++ OptionsContract.py
@@ -1,1 +1,16 @@
-print("Test")
+class Dog:
+  def init (self,name,age,gamesFetch):
+    self.name = name
+    self.age = age
+    self.gamesFetch = gamesFetch
+    
+  def whoIs(self):
+    print("Name: " + self.name)
+    print("Age: " + str(self.age))
+    print("Games of Fetch Played: " + str(self.gamesFetch))
+  
+  def birthday(self):
+    self.birthday = self.birthday + 1
+  
+  def playFetch(self,numGames): 
+    self.gamesFetch = self.gamesFetch + numGames
