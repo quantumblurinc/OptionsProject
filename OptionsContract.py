6b6df91d56d227a3f4a4ea8935a1b59e2cd3ef65
MaxBabka <maxbabka8@gmail.com>
2018-11-18 18:34:20 -0500

    Added arguments to the constructer and a method called quote which prints out basic information on the class.

--- OptionsContract.py
+++ OptionsContract.py
@@ -1,16 +1,27 @@
 class OptionsContract:
-  def init (self,callOrPut,ticker,strikePrice,contractPrice,underlyingPrice,theta,delta,daysToExpiration):
-    self.callOrPut = callOrPut
+  def init (self,ticker,strikePrice,contractPrice,underlyingPrice,daysToExpiration):
+    self.ticker = ticker
       #please add these for each variable..
     
   def quote(self):
-    print("This is a $" + str(self.strikePrice))+" "+self.callOrPut+" for $"+self.ticker+".")
+    print("This is a $" + str(self.strikePrice))+" for $"+self.ticker+".")
     print(self.ticker+" currently trades at $"+str(self.underlyingPrice)+" per share, and the contract is worth $"+str(self.contractPrice)+".")
     print("The option expires in "+str(self.daysToExpiration)+" days.")
 
   
-  def birthday(self):
-    self.birthday = self.birthday + 1
+  def moneyness(self):
+    if self.strikePrice > self.underlyingPrice:
+      intrinsicValue = self.underlyingPrice-self.strikePrice
+      print("The contract is in the money and has an intrinsic value of "+str(intrinsicValue)+'.')
+      print("It's time value is "+str(self.contractPrice-intrinsicValue)+'.')
+    else:
+      print("The contract is out of the money and has a time value of"+str(self.contractPrice)+'.')
   
-  def playFetch(self,numGames): 
-    self.gamesFetch = self.gamesFetch + numGames
+  def decay(self):
+    self.daystoExpiration = self.daystoExpiration-1
+
+  def weekly(self):
+    if self.daystoExpiration <= 5:
+      print("This contract is a weekly contract and will expire soon.")
+    else:
+      print("This is not a weekly contract.")
